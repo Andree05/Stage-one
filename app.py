f2843e08152783858f8ef4a42b49e5261ad41659
Andrew <90256093+Andree05@users.noreply.github.com>
2023-09-09 10:38:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_andrew():
         return jsonify({'Status Code': 'Invalid UTC Offset'}), 400
 
     # Get GitHub URLs
-    file_url = 'https://github.com/Andree05/Stage-One-/app.py'
+    file_url = 'https://github.com/Andree05/Stage-One/app.py'
     source_code_url = 'https://github.com/Andree05/Stage-One/'
 
     # Response data
@@ -34,6 +34,49 @@ def get_andrew():
         'Track': track,
         'GitHub URL of the file being run': file_url,
         'GitHub URL of the full source code': source_code_url,
+        'Status Code': 'Success'
+    }
+
+    return jsonify(response_data), 200
+
+#New route for publicly accessible endpoint
+@app.route('/public_endpoint', methods=['GET'])
+def get_public_info():
+    #Parse query parameters
+    slack_name = request.args.get('slack_name', 'Fagbemi Andrew')
+    track = request.args.get('track', 'Backend')
+
+    #Create a response dictionary
+    response_data = {
+        'Slack name': slack_name,
+        'Track': track
+    }
+
+    return jsonify(response_data), 200
+
+@app.route('/api', methods=['GET'])
+def get_info():
+    # Parse query parameters
+    slack_name = request.args.get('slack_name', 'Fagbemi Andrew')
+    track = request.args.get('track', 'Backend')
+
+    # Get current day of the week
+    day_of_week = datetime.datetime.utcnow().strftime('%A')
+
+    # Get current UTC time with validation of +/-2
+    utc_time = datetime.datetime.utcnow()
+    utc_offset = int(request.args.get('utc_offset', '0'))
+    if abs(utc_offset) <= 2:
+        utc_time += datetime.timedelta(hours=utc_offset)
+    else:
+        return jsonify({'Status Code': 'Invalid UTC Offset'}), 400
+
+    # Response data
+    response_data = {
+        'Slack name': slack_name,
+        'Track': track,
+        'Current day of the week': day_of_week,
+        'Current UTC time': utc_time.strftime('%Y-%m-%d %H:%M:%S'),
         'Status Code': 'Success'
     }
 
